41942e7d31632a8dc82ffb27d98b0145361cc4c6
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-19 01:43:38 +0300

    add Dop

---
+++ Dop/Program.cs
@@ -0,0 +1,42 @@
+﻿int a = ReadInt("Введите А: ");// сторона прямоугольника
+int b = ReadInt("Введите B: ");// сторона прямоугольника
+int c = ReadInt("Введите C: ");// сторона квадрата
+
+if (ABC(a, b, c))
+{
+    System.Console.WriteLine($"Квадрат со стороной {c} не влезет в прямоугольник {a}x{b}.");
+}
+else
+{
+    System.Console.WriteLine($"Квадрат со стороной {c} поместится в прямоугольник {a}x{b} {Area(a, b, c)} раз(а).");
+}
+
+int ReadInt(string argument)
+{
+	Console.Write(argument);
+
+	int i;
+
+	while (!int.TryParse(Console.ReadLine(), out i))
+	{
+        Console.WriteLine("Это не число!");
+        Console.Write(argument);
+        
+	}
+
+	return i;
+}
+bool ABC(int a, int b, int c)
+{
+    if (c < a || c < b)
+    {
+        return false;
+    }
+    return true;
+}
+int Area(int a, int b, int c)
+{
+    int count = 0;
+    count = (a * b) / (c * c);
+    return count;
+}   
